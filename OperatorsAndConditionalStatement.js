36211a4e544aafb59a6c7b55810ecbcc13627de1
JUHI NAGPURE <juhinagpure143@gmail.com>
2023-12-07 19:42:41 +0530

    added new file Operators andConditional Statements

---
+++ OperatorsAndConditionalStatement.js
@@ -0,0 +1,13 @@
+// Arithmetic operators
+let a = 5;
+let b = 2;
+
+console.log("a = ", a, "& b = ", b);
+console.log("a + b = ", a + b);
+console.log("a - b = ", a - b);
+console.log("a * b = ", a * b);
+console.log("a / b = ", a / b);
+console.log("a % b = ", a % b);
+console.log("a ** b = ", a ** b);
+
+//Unary Operator
