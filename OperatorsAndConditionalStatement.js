113641566679862f8fbe1581a8b1aa9ea0f9d959
JUHI NAGPURE <juhinagpure143@gmail.com>
2023-12-07 22:06:40 +0530

    added conditional statement

--- OperatorsAndConditionalStatement.js
+++ OperatorsAndConditionalStatement.js
@@ -2,7 +2,7 @@
 let a = 5;
 let b = 2;
 
-console.log("a = ", a, "& b = ", b);
+// console.log("a = ", a, "& b = ", b);
 // console.log("a + b = ", a + b);
 // console.log("a - b = ", a - b);
 // console.log("a * b = ", a * b);
@@ -10,6 +10,34 @@ console.log("a = ", a, "& b = ", b);
 // console.log("a % b = ", a % b);
 // console.log("a ** b = ", a ** b);
 
+// Unary Operator
 // console.log("++a=", ++a);
-console.log("a++=", a++);
-console.log(a);
+// console.log("a++=", a++);
+// console.log(a);
+
+// console.log("--a=", --a);
+// console.log("a--=", a--);
+// console.log(a);
+
+// Assignment Operator
+
+// a += 4; // a=a+4
+// console.log("a =", a);
+
+// a -= 4; // a=a-4
+// console.log("a = ", a);
+
+// a *= 4;
+// console.log("a = ", a);
+
+a /= 4;
+console.log("a = ", a);
+
+// Conditional Statements
+//  - To implement some condition in the code
+
+let age = 25;
+
+if (age > 18) {
+  console.log("you can vote");
+}
